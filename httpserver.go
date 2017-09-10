1760194e306583c8430260f0947f6205ca1d3828
evildecay <81061812@qq.com>
2017-09-10 16:01:01 +0800

    Merge the etcdkeeper, etcdkeeper3 project

--- httpserver.go
+++ httpserver.go
@@ -5,12 +5,14 @@ import(
 	"log"
 	"net/http"
 	"os"
+	"io/ioutil"
+	"strings"
 	"time"
 	"flag"
 	"strconv"
+	"encoding/json"
 	"github.com/coreos/etcd/clientv3"
 	"golang.org/x/net/context"
-	"encoding/json"
 )
 
 var cli *clientv3.Client
@@ -18,8 +20,13 @@ var cli *clientv3.Client
 func main() {
 	host := flag.String("h","0.0.0.0","host name or ip address")
 	port := flag.Int("p", 8080, "port")
+	name := flag.String("n", "/request", "request root name")
 	flag.CommandLine.Parse(os.Args[1:])
 
+	// v2
+	http.HandleFunc(*name, v2request)
+
+	// v3
 	http.HandleFunc("/connect", connect)
 	http.HandleFunc("/put", put)
 	http.HandleFunc("/get", get)
@@ -29,12 +36,42 @@ func main() {
 	if err != nil{
 		log.Fatal(err)
 	}
-	http.Handle("/", http.FileServer(http.Dir(wd))) // view static directory
+
+	log.Println(http.Dir(wd + "/assets"))
+
+	http.Handle("/", http.FileServer(http.Dir(wd + "/assets"))) // view static directory
 
 	log.Printf("listening on %s:%d\n", *host, *port)
 	err = http.ListenAndServe(*host + ":" + strconv.Itoa(*port), nil)
 	if err != nil {
 		log.Fatal(err)
+	}
+}
+
+func v2request(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		log.Println(err.Error())
+	}
+	log.Println(r.Method, "v2", r.FormValue("url"), r.PostForm.Encode())
+
+	body := strings.NewReader(r.PostForm.Encode())
+	req, err := http.NewRequest(r.Method, r.Form.Get("url"), body)
+	if err != nil {
+		io.WriteString(w, err.Error())
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	client := &http.Client{Timeout: 10*time.Second} // important!!!
+	resp, err := client.Do(req)
+	if err != nil {
+		io.WriteString(w, err.Error())
+	}else {
+		result, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			io.WriteString(w, "Get data failed: " + err.Error())
+		}else {
+			io.WriteString(w, string(result))
+		}
 	}
 }
 
@@ -57,8 +94,10 @@ func connect(w http.ResponseWriter, r *http.Request){
 		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
+		log.Println(r.Method, "v3", "connect fail.")
 		io.WriteString(w, string(err.Error()))
 	}else {
+		log.Println(r.Method, "v3", "connect success.")
 		io.WriteString(w, "ok")
 	}
 }
@@ -67,7 +106,7 @@ func put(w http.ResponseWriter, r *http.Request){
 	key := r.FormValue("key")
 	value := r.FormValue("value")
 	ttl := r.FormValue("ttl")
-	log.Println("PUT", key)
+	log.Println("PUT", "v3", key)
 
 	var err error
 	data := make(map[string]interface{})
@@ -95,7 +134,7 @@ func put(w http.ResponseWriter, r *http.Request){
 				node["key"] = string(kv.Key)
 				node["value"] = string(kv.Value)
 				node["dir"] = false
-				node["ttl"] = 0 // TODO: clientv3.0 not support
+				node["ttl"] = 0 // FIXME: clientv3.0 not support?
 				node["createdIndex"] = kv.CreateRevision
 				node["modifiedIndex"] = kv.ModRevision
 				data["node"] = node
@@ -113,7 +152,8 @@ func put(w http.ResponseWriter, r *http.Request){
 func get(w http.ResponseWriter, r *http.Request){
 	key := r.FormValue("key")
 	data := make(map[string]interface{})
-	log.Println("GET", key)
+	log.Println("GET", "v3", key)
+
 	if resp, err := cli.Get(context.Background(), key, clientv3.WithPrefix());err != nil {
 		data["errorCode"] = err.Error()
 	}else {
@@ -126,7 +166,7 @@ func get(w http.ResponseWriter, r *http.Request){
 				node["key"] = string(kv.Key)
 				node["value"] = string(kv.Value)
 				node["dir"] = false
-				node["ttl"] = 0 // TODO: clientv3.0 not support
+				node["ttl"] = 0 // FIXME: clientv3.0 not support?
 				node["createdIndex"] = kv.CreateRevision
 				node["modifiedIndex"] = kv.ModRevision
 				nodes := pnode["nodes"].([]map[string]interface{})
@@ -140,7 +180,7 @@ func get(w http.ResponseWriter, r *http.Request){
 				node["key"] = string(kv.Key)
 				node["value"] = string(kv.Value)
 				node["dir"] = false
-				node["ttl"] = 0 // TODO: clientv3.0 not support
+				node["ttl"] = 0 // FIXME: clientv3.0 not support?
 				node["createdIndex"] = kv.CreateRevision
 				node["modifiedIndex"] = kv.ModRevision
 				data["node"] = node
@@ -160,7 +200,8 @@ func get(w http.ResponseWriter, r *http.Request){
 
 func del(w http.ResponseWriter, r *http.Request){
 	key := r.FormValue("key")
-	log.Println("DELETE", key)
+	log.Println("DELETE", "v3", key)
+
 	if _, err := cli.Delete(context.Background(), key);err != nil {
 		io.WriteString(w, err.Error())
 	}else {
